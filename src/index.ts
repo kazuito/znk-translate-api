fa38c9337e3060dcc6578527fb6d16e08673bea8
Kazuma Ito <zarigani2x@gmail.com>
2024-03-24 10:19:07 +0900

    Refactor server startup code

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ app.post("/translate", async (req, res) => {
   }
 });
 
-const server = app.listen(Number(process.env.PORT), "0.0.0.0", () =>
+app.listen(Number(process.env.PORT), "0.0.0.0", () =>
   console.log(`Server is running on port ${process.env.PORT}...`)
 );
 
